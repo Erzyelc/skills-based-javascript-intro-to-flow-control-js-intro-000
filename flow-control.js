635d5e3664be1ec1ff8bc602b3b4aca4a2978e17
cheerful-architect-4052 <scrian_12@yahoo.com>
2020-05-19 19:02:53 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,10 +1,8 @@
 function basicTeenager(age) {
   if(age >= 13 && age <= 19){
-    console.log('You are a teenager!');
+    return 'You are a teenager!';
   }
 }
-
-basicTeenager(19);
 
 function teenager(age) {
   if(age >= 13 && age <= 19){
